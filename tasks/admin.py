17493bc95c004a641aefd3161b6d4a6c47dede1e
abbosbek <hikmatullaevabbos24@gmail.com>
2024-09-18 04:47:27 -0400

    Bismillah

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Tasks
+from .models import Tasks, Comment
 
-admin.site.register(Tasks)
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['id', "author", 'title', 'status', 'due_date']
+    list_display_links = ['id', 'author', 'title']
+    search_fields = ['author', 'title']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'task', 'created_at']
+    list_display_links = ['id', 'author']
+    search_fields = ['author', 'task']
--- tasks/models.py
+++ tasks/models.py
@@ -25,3 +25,19 @@ class Tasks(models.Model):
         ordering = ('created_at',)
         verbose_name = "Task"
         verbose_name_plural = "Tasks"
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=400)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author
+
+    class Meta:
+        ordering = ('created_at',)
+        verbose_name = 'Comment',
+        verbose_name_plural = "Comments"
